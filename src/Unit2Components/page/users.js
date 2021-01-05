5b893d3d81cfcf2a04fb094fcf33666ef5c165c8
Justin Justin <jbry0723@gmail.com>
2021-01-05 05:45:54 -0500

    added item and user cards render, added click functionality to users, added API call scaffolding

--- src/Unit2Components/page/users.js
+++ src/Unit2Components/page/users.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import './cssPage/users.css'
 import Header from './Header'
 
 function Users () {
+    const testUsers=[{name:"Billy",email:"billy@bill.com", id:5, location:"place", password:"353532523523#%#@%@#%", username:'TOM'},
+    {name:"Lilly",email:"Lilly@bill.com", id:7, location:"home", password:"353532523523#%#@%@#%", username:'Lill'}]
+    const [usersArray, setUsersArray]= useState(testUsers)
+    const [selectedUser,setSelectedUser]=useState(null)
+
+    useEffect(()=>{
+        //API call goes here
+    })
+
+    const openDetails=(id)=>{
+        if (selectedUser===id){setSelectedUser(null)}
+        else {setSelectedUser(id)}
+    }
 
         return(
             <div className='homeBody userBckg'>
@@ -18,7 +31,22 @@ function Users () {
                     </p>
                     <div className='users-container'>
                         <section className='user-list'>
-                            
+                        {usersArray.map(item=>{
+                               return <div onClick={()=>openDetails(item.id)}>
+                                   <h3 className={selectedUser===item.id ? "selectedUserNameDiv" : "userNameDiv"}>{item.username}</h3>
+                                {selectedUser===item.id &&
+                                <div className="detailsDiv">
+                                <p>Name: {item.name}</p>
+                                <p>Email: {item.email}</p>
+                                <p>Location: {item.location}</p>
+                                
+                                
+                                </div>}
+
+
+                                </div>
+                                
+                            })}
                                    
                             
                         </section>
